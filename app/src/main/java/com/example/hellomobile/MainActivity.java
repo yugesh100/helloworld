1bbd3b01692c24544b310442c4ae6fbc5e11c05c
yugesh <ahltoea@gmail.com>
2020-02-17 20:58:21 +0545

    hello mobile,Toast button  and count

--- app/src/main/java/com/example/hellomobile/MainActivity.java
+++ app/src/main/java/com/example/hellomobile/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.hellomobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button countButton;
+    private Button toastButton;
+    private TextView textView;
+    private int count;
+    public static final String TAG= "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        countButton = findViewById(R.id.button1);
+        toastButton = findViewById(R.id.toggleButton1);
+        textView = findViewById(R.id.textView2);
+
+        countButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "This is a toast as yugesh sapkota", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
     }
+
+    public void count(View view) {
+       count++;
+       textView.setText(""+count);
+       Log.d(TAG,""+count);
+
+          }
 }
+//
